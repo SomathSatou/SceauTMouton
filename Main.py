415400e61849568534141174e4961364749c61b1
Satou <thomas.saout.pro@gmail.com>
2020-01-14 16:15:05 +0100

    initial commit

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
-import tkinter
+from tkinter import *
 
 def mainwindow():
-    
+    fenetre = Tk()
+
+    label = Label(fenetre, text="SceauTMouton")
+    label.pack()
+
+    fenetre.mainloop()
     return
 
 
