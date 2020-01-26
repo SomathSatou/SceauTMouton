8a07cdbad98cc46f5f6a1ef0a761410472c43d2f
Cyril Lepinette <lepinette.cyril@gmail.com>
2020-01-26 14:45:51 +0100

    fix

--- class_moutons.py
+++ class_moutons.py
@@ -15,22 +15,39 @@ class Mouton:
 
 
 class Modelisation_Mouton:
-    def __init__(self, liste_moutons):
+    def __init__(self, nombre_moutons):
 
         # Create a MiniZinc model
         self.model = mnz.Model()
-        #self.model.add_file("moutons.mzn")
+        self.model.add_file("ProjSauteMouton.mzn")
 
         # Transform Model into a instance
         gecode = mnz.Solver.lookup("gecode")
         self.inst = mnz.Instance(gecode, self.model)
 
-        self.moutons = liste_moutons
 
+        self.nb_moutons = nombre_moutons
+        self.inst["nbr_m"] = nombre_moutons
+
+        self.moutons = []
+
+        '''
+        self.inst["nbr_sceau"] = len(liste_gallons)
+        self.inst["remplissage_final"] = quantite_objectif
+
+        list_taille_sceau = []
+        list_remplissage_init = []
+        for g in self.gallons:
+            list_taille_sceau.append(g.taille)
+            list_remplissage_init.append(g.remplissage)
+
+        self.inst["taille_sceau"] = list_taille_sceau
+        self.inst["remplissage_initial"] = list_remplissage_init
+        '''
 
 
     def __str__(self):
-        return str(self.moutons)
+        return "bla" #str(self.moutons)
 
     def __repr__(self):
         return self.__str__()
@@ -42,21 +59,28 @@ class Modelisation_Mouton:
             print("PAS DE SOLUTION")
             return False
         else:
-            '''
-            #for i in range(len(result)):
-                #print(result["remplissage"])
-                #print(result["unsolved_step"])
+
+            for i in range(len(result)):
+                print(result["etat"])
+                print(result["unsolved_step"])
 
             index = 0
             while result["unsolved_step"][index]==1:
                 index+=1
 
-            gallon_number=0
-            for g in self.gallons:
-                g.remplissage = result["remplissage"][index][gallon_number]
-                gallon_number+=1
+
+            swap_sol=0
+            #for sols in result["etat"]:
+            final_list = []
+            for i in range(len(result["unsolved_step"])):
+                final_list.append(result["etat"][i*(self.nb_moutons*2+1):(i+1)*(self.nb_moutons*2+1)])
+            #good_sub_list = result["etat"][index*(self.nb_moutons*2+1):(index+1)*(self.nb_moutons*2+1)]
+
+            print(final_list)
+            #self.moutons =
+
 
             print("\nSOLUTION : ")
-            print(self.gallons)
-            '''
+            #print(self.gallons)
+
             return True
